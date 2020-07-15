0915d78138fc1f3f6f150f46460992ab32967d68
Mario Andres <marioarendon2@gmail.com>
2020-07-15 15:07:41 -0600

    Initial Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,73 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import { useColorMode, Flex, Box } from "theme-ui"
+import { IconButton } from "theme-ui"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  const [colorMode, setColorMode] = useColorMode()
+  return (
+    <header>
+      <Flex
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <Box p={2} sx={{ flex: "1 1 auto" }}>
+          <Link
+            to="/"
+            sx={{
+              mr: 16,
+              textDecoration: `none`,
+              textTransform: "uppercase",
+            }}
+          >
+            {siteTitle}
+          </Link>
+
+          <Link
+            to="/blog/"
+            sx={{
+              textDecoration: `none`,
+              textTransform: "uppercase",
+            }}
+          >
+            Blog
+          </Link>
+        </Box>
+
+        <Box p={2}>
+          <IconButton
+            aria-label="Toggle Theme Mode"
+            onClick={e => {
+              setColorMode(colorMode === "default" ? "dark" : "default")
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="currentcolor"
+            >
+              <circle
+                r={11}
+                cx={12}
+                cy={12}
+                fill="none"
+                stroke="currentcolor"
+                strokeWidth={2}
+              />
+            </svg>
+          </IconButton>
+        </Box>
+      </Flex>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
