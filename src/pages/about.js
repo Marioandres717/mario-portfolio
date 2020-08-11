2dcf9a451251411017acaca4bdc9dcaf4e365882
Mario Andres <marioarendon2@gmail.com>
2020-08-11 16:28:52 -0600

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <h1>ABOUT ME</h1>
+  </Layout>
+)
+
+export default About
