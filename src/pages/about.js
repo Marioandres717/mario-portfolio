ccd4f0c30c1930c3978967a2dcde1b518b5cf9c9
Mario Andres <marioarendon2@gmail.com>
2020-08-12 00:34:03 -0600

    add about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,20 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx, Styled } from "theme-ui"
 import Layout from "../components/layout"
 
 const About = () => (
   <Layout>
-    <h1>ABOUT ME</h1>
+    <div sx={{ maxWidth: 768 }}>
+      <Styled.h1>ABOUT</Styled.h1>
+      <Styled.p>
+        I’m a full-stack JavaScript Engineer who’s passionate about web
+        development and the impact it has on the world. In my free time, I
+        always upgrade my knowledge by searching for the latest trends on
+        Javascript. I strive for building modern web applications that deliver
+        maximum efficiency in everyday life and challenge my knowledge while
+        inspiring my creativity.
+      </Styled.p>
+    </div>
   </Layout>
 )
 
