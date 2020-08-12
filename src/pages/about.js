d28c255439a42c030f18274b5ce595b5856e353a
Mario Andres <marioarendon2@gmail.com>
2020-08-12 02:38:19 -0600

    Style page component

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,18 +6,18 @@ import Image from "../components/image"
 const About = () => (
   <Layout>
     <div sx={{ maxWidth: 768 }}>
-      <Styled.h1>ABOUT</Styled.h1>
+      <h1 sx={{ fontSize: 36 }}>About</h1>
       <div sx={{ maxWidth: 500 }}>
         <Image />
       </div>
-      <Styled.p>
+      <p sx={{ fontSize: 18 }}>
         I’m a full-stack JavaScript Engineer who’s passionate about web
         development and the impact it has on the world. In my free time, I
         always upgrade my knowledge by searching for the latest trends on
         Javascript. I strive for building modern web applications that deliver
         maximum efficiency in everyday life and challenge my knowledge while
         inspiring my creativity.
-      </Styled.p>
+      </p>
     </div>
   </Layout>
 )
