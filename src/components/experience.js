9881ff95c6660b77a87f51aec2b5b0b7ed04957a
Mario Andres <marioarendon2@gmail.com>
2022-02-14 03:58:30 -0600

    remove non-professional experience

--- src/components/experience.js
+++ src/components/experience.js
@@ -23,27 +23,6 @@ const Experience = () => (
         allow organizations to collect data and visualize critical metrics about
         its business
       </li>
-
-      <li>
-        {" "}
-        <a href="https://tricky-game.herokuapp.com/login">
-          {" "}
-          <h3>Tic Tac Toe Game</h3>
-        </a>
-        I created an online tic-tac-toe game that leverages{" "}
-        <a href="https://firebase.google.com/">Firebase</a> as the real-time
-        database and <a href="https://socket.io/">WebSockets</a> for multiplayer
-        functionality
-      </li>
-
-      <li>
-        {" "}
-        <a href="http://www2.cs.uregina.ca/~rendon2m/v3/login.php">
-          <h3> Twitter Clone</h3>
-        </a>
-        I created a twitter-like application where the user can post messages
-        and pictures, and other users can comment on those posts.
-      </li>
     </ul>
   </Fragment>
 )
