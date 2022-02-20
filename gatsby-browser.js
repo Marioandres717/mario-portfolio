4da89ba2fabf27899f49638cb945fd31a808671d
Mario Andres <marioarendon2@gmail.com>
2022-02-20 17:13:55 -0600

    style Header.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import "./src/styles/css-reset.css"
 import "./src/styles/theme.css"
+import "./src/styles/base.css"
--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,42 @@ import React, { useState } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 
-const Header = ({ siteTitle }) => {
+import "../styles/header.module.css"
+
+const Header = () => {
   const [colorMode, setColorMode] = useState()
   return (
     <header>
       <div>
-        <div>
-          <Link to="/">Home</Link>
-        </div>
-
-        <div>
-          <button
-            aria-label="Toggle Theme Mode"
-            onClick={e => {
-              setColorMode(colorMode === "default" ? "dark" : "default")
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              fill="currentcolor"
-            >
-              <circle
-                r={11}
-                cx={12}
-                cy={12}
-                fill="none"
-                stroke="currentcolor"
-                strokeWidth={2}
-              />
-            </svg>
-          </button>
-        </div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
       </div>
+
+      <button
+        title="Cycle Color Mode"
+        aria-label="Toggle Theme Mode"
+        onClick={() => {
+          setColorMode(colorMode === "default" ? "dark" : "default")
+        }}
+      >
+        <svg viewBox="0 0 32 32" width="24" height="24" fill="currentcolor">
+          <circle
+            r={14}
+            cx={16}
+            cy={16}
+            fill="none"
+            stroke="currentcolor"
+            strokeWidth={4}
+          />
+          <path
+            d="
+                    M 16 0
+                    A 16 16 0 0 0 16 32
+                    z
+          "
+          ></path>
+        </svg>
+      </button>
     </header>
   )
 }
